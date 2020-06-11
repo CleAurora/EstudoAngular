716ff25a5e697032bb88bb99b9d56b308ef6663e
Cleis Pereira <cleispereira@gmail.com>
2020-06-11 18:24:05 -0300

    estudos de angular na DIO aula 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // diretiva que vai virar uma tag html
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'course-manager';
+
+  name: string = "Cleís"; 
 }
---
+++ src/app/courses/course.ts
@@ -0,0 +1,10 @@
+export class Course{
+    id: number;
+    name: string;
+    imageUrl: string;
+    price: number;
+    code: string;
+    durarion: number;
+    rating: number;
+    releaseDate: string;
+}
