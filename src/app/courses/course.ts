f88f684d8177fdd480857ab0361a722f282e94f7
Cleis Pereira <cleispereira@gmail.com>
2020-06-14 18:24:57 -0300

    Mexe com rotas e adiciona form

--- src/app/courses/course.ts
+++ src/app/courses/course.ts
@@ -4,7 +4,8 @@ export class Course{
     imageUrl: string;
     price: number;
     code: string;
-    durarion: number;
+    duration: number;
     rating: number;
     releaseDate: string;
+    description: string;
 }
